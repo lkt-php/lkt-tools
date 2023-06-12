07aeb38f738aa095dec064721d9ddd0973a856b2
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2023-06-12 18:05:53 +0200

    Add utf8ize function

--- src/Requiring/requireFiles.php
+++ src/Requiring/requireFiles.php
@@ -2,17 +2,19 @@
 
 namespace Lkt\Tools\Requiring;
 
-function requireFiles(array $paths): void
-{
-    foreach ($paths as $path) {
-        $files = glob($path);
-        if ($files === false) {
-            throw new \RuntimeException("Failed to glob for function files");
+if (!function_exists('\Lkt\Tools\Requiring\requireFiles')) {
+    function requireFiles(array $paths): void
+    {
+        foreach ($paths as $path) {
+            $files = glob($path);
+            if ($files === false) {
+                throw new \RuntimeException("Failed to glob for function files");
+            }
+            foreach ($files as $file) {
+                require_once $file;
+            }
+            unset($file);
+            unset($files);
         }
-        foreach ($files as $file) {
-            require_once $file;
-        }
-        unset($file);
-        unset($files);
     }
 }
