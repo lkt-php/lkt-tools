2722634c22aa86c1676008c26c9c43df00e5bfb6
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-01-12 17:20:43 +0100

    Add sanitizeMailContent function

---
+++ src/Strings/sanitizeMailContent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lkt\Tools\Strings;
+
+function sanitizeMailContent(string $str = ''): string
+{
+    $search = [
+        '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+        '/(\s)+/s',         // shorten multiple whitespace sequences
+        '/<!--(.|\s)*?-->/' // Remove HTML comments
+    ];
+
+    $replace = [
+        '>',
+        '<',
+        '\\1',
+        ''
+    ];
+
+    return preg_replace($search, $replace, $str);
+}
